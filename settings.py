af0ed8e8f2c5a55cb9e9eec6644ab8bd6c2c95ff
Eduardo Sebastian <esebastian@altiora.es>
2018-11-08 23:59:28 +0100

    Remove obsolete setting
    civio/presupuesto-management#603

--- settings.py
+++ settings.py
@@ -167,8 +167,6 @@ OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '01', '33', '32', '17']
 # Nodes ordered by amount by default. If set to True keeps the order defined in nodes array. Default: False (Optional)
 # OVERVIEW_FORCE_ORDER = False
 
-# Adjust inflation in amounts in Overview page. Default: True
-# ADJUST_INFLATION_IN_OVERVIEW = False
 
 # Show Subtotals panel in Overview. Default: False
 # SHOW_OVERVIEW_SUBTOTALS = True
